079f322773149c62e9561f5a4cb45e6cfc5b87b2
Agnieszka Bielecka <abielecka109@gmail.com>
2023-03-18 20:49:16 +0100

    export created dataset

---
+++ SQL Dataset/staff_202303182047.sql
@@ -0,0 +1,6 @@
+INSERT INTO school_library.staff (staff_firstName,staff_lastName,email,password) VALUES
+	 ('Renata','Mazur','renata_mazur_liber@gmail.com','mazurek123'),
+	 ('Stanisław','Kowalski','stanislaw_kowalski_liber@gmail.com','tolkien102#'),
+	 ('Katarzyna','Walczak','katarzyna_walczak_liber@gmail.com','kaśka<3'),
+	 ('Dariusz','Podlaski','dariusz_podlaski_liber@gmail.com','trudnehasło123'),
+	 ('Alicja','Olejnik','alicja_olejnik_liber@gmail.com','4d*kds43*^D0pR');
